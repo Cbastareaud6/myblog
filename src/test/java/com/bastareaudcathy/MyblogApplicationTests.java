dea63acf4d2b1a6ffa41d23a5d5deaf07132af30
Cathy Bastareaud <catybastareaud@gmail.com>
2022-05-15 10:34:51 -0600

    Initial commit

---
+++ src/test/java/com/bastareaudcathy/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bastareaudcathy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
